9a5675f139d772645e803dedceef9a0e92c33dc6
AdleyTales <AdleyTales@git config --global user.email AdleyTales@126.com>
2017-10-20 23:30:42 +0800

    -v 004 python高级特性

---
+++ 3_python高级特性.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+
+l = ['hsfkdj','1232','true','nnn','mmmm',1212]
+print(l[:2]) #前两项
+print(l[-3:]) #后边3项
+
+print('#########################')
+
+t = list(range(100))
+print(t[:20:2])
+
+print('#########################')
+
+s = 'jdskfjksdfjks'
+print(s[2:6])
+
+print('#########################')
+y = (1,2,34,5,6,7,8,9,343)
+print(y[-4:]) #还是元祖
+
+print('#########################')
+# 迭代
+d = {'name':'xiaoming','age':123,'salary':15000}
+for key in d:
+    print(key)
+
+print('#########################')
+for value in d.values():
+    print(value)
+
+print('#########################')
+for k,v in d.items():
+    print('k:',k,'v:',v)
